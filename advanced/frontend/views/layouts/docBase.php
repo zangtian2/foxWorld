817e0064d9fbc4151e3a3523a894063664a59280
Bob <Bob@PreFU>
2017-04-11 01:42:09 +0800

    优化学习区界面 初步合入editor.md

--- advanced/frontend/views/layouts/docBase.php
+++ advanced/frontend/views/layouts/docBase.php
@@ -1,9 +1,7 @@
 <?php
 /* @var $this yii\web\View */
 $this->beginContent('@app/views/layouts/main.php');
-
 use frontend\assets\AppAsset;
-
 AppAsset::register($this);
 AppAsset::addCss($this, "@web" . "/css/zidingyi.css");
 AppAsset::addCss($this, "@web" . "/css/zidingyi/icon/ionicons.css");
@@ -13,75 +11,66 @@ AppAsset::addCss($this, "@web" . "/css/navFox.css");
 AppAsset::addScript($this, "@web" . "/js/learnCenter.js");
 ?>
 
-
-
-<div  class="feng-nav" > 
-
-    <!-- <nav class=""> -->
+<div  class="feng-nav" >
     <div class="feng-nav-left-top">
-    <header class="feng-nav-header">
-        <h2 class="feng-nav-title" style="letter-spacing: 3px;font-size: 16px;
-line-height: 20px;"> Spiritual Warfare</h2>
-    </header>
+        <header class="feng-nav-header">
+            <h2 class="feng-nav-title"> Spiritual Warfare</h2>
+        </header>
         <div class="feng-nav-category">
-        <ul class="layui-nav  my-nav-side"  style="background-color: transparent;">
-            <!-- 侧边导航: <ul class="layui-nav layui-nav-tree layui-nav-side"> -->
-            <li class=" drop-show feng-nav-item u-category-windows sub-menu">
-                <a href="javascript:;" >
-                    <i class="icon ion-monitor"></i>
-                    <span class="nav-text">窗体<i class="layui-icon">&#xe602;</i>  </span>
-
-                </a>
-                <ul class="sub drop-list" style="display:block">
-                    <li>      <button type="button" id="button-windows" data-section="windows" class="feng-nav-button is-selected">创建并管理 <em>窗体</em></button></li>
-                    <li>      <button type="button" id="button-crash-hang" data-section="crash-hang" class="feng-nav-button">处理窗体 <em>崩溃和挂起</em></button></li>
-                </ul>
-            </li>
-
-            <li class="drop-show feng-nav-item u-category-menu sub-menu">
-                <a href="javascript:;" >
-                    <i class="icon ion-monitor"></i>
-                    <span class="nav-text">菜单<i class="layui-icon">&#xe602;</i></span>
-
-                </a>
-                <ul class="sub drop-list">
-                    <li><button type="button" id="button-menus" data-section="menus" class="feng-nav-button">自定义 <em>菜单</em> </button></li>
-                    <li><button type="button" id="button-shortcuts" data-section="shortcuts" class="feng-nav-button">注册键盘 <em>快捷键</em></button></li>
-
-                </ul>
-            </li>
-
-
-
-            <li class="drop-show feng-nav-item u-category-native-ui sub-menu">
-                <a href="javascript:;" >
-                    <i class="icon ion-monitor"></i>
-                    <span class="nav-text">本地用户界面<i class="layui-icon">&#xe602;</i></span>
-
-                </a>
-                <ul class="sub drop-list">
-                    <li><button type="button" id="button-ex-links-file-manager" data-section="ex-links-file-manager" class="feng-nav-button">打开 <em>外部链接</em> 或 系统 <em>文件管理器</em></button></li>
-                    <li><button type="button" id="button-dialogs" data-section="dialogs" class="feng-nav-button">使用系统 <em>对话框</em></button></li>
-                    <li><button type="button" id="button-tray" data-section="tray" class="feng-nav-button">将应用程序放入 <em>托盘</em></button></li>
-
-                </ul>
-            </li>
-
-            <li class="drop-show feng-nav-item u-category-windows sub-menu">
-                <a href="javascript:;" >
-                    <i class="icon ion-monitor"></i>
-                    <span class="nav-text">窗体<i class="layui-icon">&#xe602;</i></span>
-
-                </a>
-                <ul class="sub drop-list">
-                    <li>      <button type="button" id="button-windows" data-section="windows" class="feng-nav-button">创建并管理 <em>窗体</em></button></li>
-                    <li>      <button type="button" id="button-crash-hang" data-section="crash-hang" class="feng-nav-button">处理窗体 <em>崩溃和挂起</em></button></li>
-                </ul>
-            </li>
-        </ul>
-
-    </div>
-        
+            <ul class="my-nav-side">
+                <li class="feng-nav-item u-category-windows">
+                    <a href="javascript:;" class="feng-drop-a">
+                        <i class="icon ion-monitor"></i>
+                        <span class="nav-text">窗体<i class="layui-icon">&#xe602;</i>  </span>
+                    </a>
+                    <ul class="drop-list" style="display:block">
+                        <li>      <button type="button" id="button-windows" data-section="windows" class="feng-nav-button is-selected" onclick="alert(1);">创建并管理 <em>窗体</em></button></li>
+                        <li>      <button type="button" id="button-crash-hang" data-section="crash-hang" class="feng-nav-button">处理窗体 <em>崩溃和挂起</em></button></li>
+                    </ul>
+                </li>
+                <li class="feng-nav-item u-category-menu">
+                    <a href="javascript:;" class="feng-drop-a" >
+                        <i class="icon ion-monitor"></i>
+                        <span class="nav-text">菜单<i class="layui-icon">&#xe602;</i></span>
+                    </a>
+                    <ul class="drop-list">
+                        <li><button type="button" id="button-menus" data-section="menus" class="feng-nav-button">自定义 <em>菜单</em> </button></li>
+                        <li><button type="button" id="button-shortcuts" data-section="shortcuts" class="feng-nav-button">注册键盘 <em>快捷键</em></button></li>
+                    </ul>
+                </li>
+                <li class="feng-nav-item u-category-native-ui">
+                    <a href="javascript:;" class="feng-drop-a" >
+                        <i class="icon ion-monitor"></i>
+                        <span class="nav-text">本地用户界面<i class="layui-icon">&#xe602;</i></span>
+                    </a>
+                    <ul class="drop-list">
+                        <li><button type="button" id="button-ex-links-file-manager" data-section="ex-links-file-manager" class="feng-nav-button">打开 <em>外部链接</em> 或 系统 <em>文件管理器</em></button></li>
+                        <li><button type="button" id="button-dialogs" data-section="dialogs" class="feng-nav-button">使用系统 <em>对话框</em></button></li>
+                        <li><button type="button" id="button-tray" data-section="tray" class="feng-nav-button">将应用程序放入 <em>托盘</em></button></li>
+                    </ul>
+                </li>
+                <li class="feng-nav-item u-category-windows">
+                    <a href="javascript:;" class="feng-drop-a" >
+                        <i class="icon ion-monitor"></i>
+                        <span class="nav-text">窗体<i class="layui-icon">&#xe602;</i></span>
+                    </a>
+                    <ul class="drop-list">
+                        <li>      <button type="button" id="button-windows" data-section="windows" class="feng-nav-button">创建并管理 <em>窗体</em></button></li>
+                        <li>      <button type="button" id="button-crash-hang" data-section="crash-hang" class="feng-nav-button">处理窗体 <em>崩溃和挂起</em></button></li>
+                    </ul>
+                </li>
+                <li class="feng-nav-item u-category-windows" >
+                    <a href="javascript:;">
+                        <i class="icon ion-monitor"></i>
+                        <span class="nav-text">窗体<i class="layui-icon">&#xe602;</i></span>
+                    </a>
+                    <ul class="drop-list">
+                        <li>      <button type="button" id="button-windows" data-section="windows" class="feng-nav-button">创建并管理 <em>窗体</em></button></li>
+                        <li>      <button type="button" id="button-crash-hang" data-section="crash-hang" class="feng-nav-button">处理窗体 <em>崩溃和挂起</em></button></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
     <div class="feng-nav-left-bottom ">
         <div style="padding: 0px 2.3em;
@@ -91,23 +80,21 @@ line-height: 20px;"> Spiritual Warfare</h2>
             </button>
             <button id="minus" class="self-nav-footer-button">
                 <i class="icon ion-ios-minus"></i>
-            </button>       
+            </button>
             <button class="self-nav-footer-button">
                 <i data-modal="about" class="icon ion-ios-help"></i>
-            </button>      
+            </button>
             <button  id="window" name="fullScreen" class="self-nav-footer-button">
-                <i class="icon ion-android-expand"></i>      
-            </button> 
+                <i class="icon ion-android-expand"></i>
+            </button>
             <button  id="refresh" name="refresh" class="self-nav-footer-button">
-                <i class="icon ion-refresh"></i>      
-            </button> 
-        </div>  
-
+                <i class="icon ion-refresh"></i>
+            </button>
+        </div>
     </div>
 </div>
-
-
 <div class="feng-right">
-<?= $content ?>
+    <?= $content ?>
 </div>
+
 <?php $this->endContent(); ?>
--- advanced/frontend/views/layouts/main.php
+++ advanced/frontend/views/layouts/main.php
@@ -5,15 +5,11 @@
 use yii\helpers\Html;
 use frontend\assets\AppAsset;
 use yii\helpers\Url;
-
 AppAsset::register($this);
-
 $controllerID = Yii::$app->controller->id;
 $actionID = Yii::$app->controller->action->id;
 $action = $controllerID . '/' . $actionID;
 ?>
-
-
 
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -22,70 +18,65 @@ $action = $controllerID . '/' . $actionID;
         <meta charset="<?= Yii::$app->charset ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<?= Html::csrfMetaTags() ?>
-        <title><?= Html::encode($this->title) ?></title>       
-
-<?php $this->head() ?>
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
     </head>
     <body>
-<?php $this->beginBody() ?>
+        <?php $this->beginBody() ?>
 
         <div class="header">
-            <div class="main">
-                <div class="layui-clear">
-                    <a class="logo layui-this " href="/" title="Fly" >Fox社区</a>
+
+            <div class="layui-clear">
+                <a class="logo layui-this " href="/" title="Fly" >Fox社区</a>
+                <div class="nav">
+                    <ul class="layui-nav" lay-filter="">
+                        <li class="layui-nav-item nav-change <?= $action == "learn/index" ? 'layui-this' : null ?>"><a href="<?= Url::toRoute('learn/index') ?>">学习区</a></li>
+                        <li class="layui-nav-item nav-change"><a href="">书籍区</a></li>
+                        <li class="layui-nav-item nav-change <?= $action == "community/index" ? 'layui-this' : null ?>">
+                            <a href="<?= Url::toRoute('community/index') ?>">交流区</a>
+                            <dl class="layui-nav-child my-nav-child"> <!-- 二级菜单 -->
+                                <dd ><a href="">移动模块</a></dd>
+                                <dd ><a href="">后台模版</a></dd>
+                                <dd ><a href="">电商平台</a></dd>
+                            </dl>
+                        </li>
+                        <li class="layui-nav-item nav-change" ><a href="">平台区</a></li>
+                    </ul>
+                    <!--                           <a class="nav-this" href="jie/index.html">
+                                                <i class="iconfont icon-wenda"></i>问答
+                                              </a>
+                                              <a href="http://www.layui.com/" target="_blank">
+                                                <i class="iconfont icon-ui"></i>框架
+                                              </a> -->
+                </div>
+                <div class="nav-user">
+                    <!-- 未登入状态 -->
+                    <a class="unlogin" href="user/login.html"><i class="iconfont icon-touxiang"></i></a>
+                    <span><a href="user/login.html">登入</a><a href="user/reg.html">注册</a></span>
+              <!--       <p class="out-login">
+                      <a href="" onclick="layer.msg('正在通过QQ登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-qq" title="QQ登入"></a>
+                      <a href="" onclick="layer.msg('正在通过微博登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-weibo" title="微博登入"></a>
+                    </p>
+                    -->
+                    <!-- 登入后的状态 -->
+                    <!--
+                    <a class="avatar" href="user/index.html">
+                      <img src="http://tp4.sinaimg.cn/1345566427/180/5730976522/0">
+                      <cite>贤心</cite>
+                      <i>VIP2</i>
+                    </a>
                     <div class="nav">
-                        <ul class="layui-nav" lay-filter="">
-                            <li class="layui-nav-item nav-change <?= $action == "learn/index" ? 'layui-this' : null ?>"><a href="<?= Url::toRoute('learn/index') ?>">学习区</a></li>
-                            <li class="layui-nav-item nav-change"><a href="">书籍区</a></li>
-                            <li class="layui-nav-item nav-change <?= $action == "community/index" ? 'layui-this' : null ?>">
-                                <a href="<?= Url::toRoute('community/index') ?>">交流区</a>
-                                <dl class="layui-nav-child" style=" font-size: 14px;"> <!-- 二级菜单 -->
-                                    <dd style="line-height: 36px;"><a href="">移动模块</a></dd>
-                                    <dd style="line-height: 36px;"><a href="">后台模版</a></dd>
-                                    <dd style="line-height: 36px;"><a href="">电商平台</a></dd>
-                                </dl>
-                            </li>
-                            <li class="layui-nav-item nav-change" ><a href="">平台区</a></li>
-                        </ul>
-                        <!--                           <a class="nav-this" href="jie/index.html">
-                                                    <i class="iconfont icon-wenda"></i>问答
-                                                  </a>
-                                                  <a href="http://www.layui.com/" target="_blank">
-                                                    <i class="iconfont icon-ui"></i>框架
-                                                  </a> -->
-                    </div>
-
-                    <div class="nav-user">
-                        <!-- 未登入状态 -->
-                        <a class="unlogin" href="user/login.html"><i class="iconfont icon-touxiang"></i></a>
-                        <span><a href="user/login.html">登入</a><a href="user/reg.html">注册</a></span>
-                  <!--       <p class="out-login">
-                          <a href="" onclick="layer.msg('正在通过QQ登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-qq" title="QQ登入"></a>
-                          <a href="" onclick="layer.msg('正在通过微博登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-weibo" title="微博登入"></a>
-                        </p>   
-                        -->
-                        <!-- 登入后的状态 -->
-                        <!-- 
-                        <a class="avatar" href="user/index.html">
-                          <img src="http://tp4.sinaimg.cn/1345566427/180/5730976522/0">
-                          <cite>贤心</cite>
-                          <i>VIP2</i>
-                        </a>
-                        <div class="nav">
-                          <a href="/user/set/"><i class="iconfont icon-shezhi"></i>设置</a>
-                          <a href="/user/logout/"><i class="iconfont icon-tuichu" style="top: 0; font-size: 22px;"></i>退了</a>
-                        </div> -->
-
-                    </div>
+                      <a href="/user/set/"><i class="iconfont icon-shezhi"></i>设置</a>
+                      <a href="/user/logout/"><i class="iconfont icon-tuichu" style="top: 0; font-size: 22px;"></i>退了</a>
+                    </div> -->
                 </div>
             </div>
         </div>
         <div class="feng-body">
-<?= $content ?>
+            <?= $content ?>
         </div>
-  
-<?php $this->endBody() ?>
-</body>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
