2dbd3913be3c5c89ea1252599049711ba9c8aa44
fengbo <fengbo@huaqin.com>
2017-05-12 11:57:43 +0800

    二定交流区

--- advanced/frontend/views/post/index.php
+++ advanced/frontend/views/post/index.php
@@ -11,11 +11,11 @@ use yii\helpers\Url;
 
 //$this->title = '文章';
 //$this->params['breadcrumbs'][] = $this->title;
-//use frontend\assets\AppAsset;
+use frontend\assets\AppAsset;
 //
-//AppAsset::register($this);
+AppAsset::register($this);
 //
-//AppAsset::addScript($this,"http://cdn.bootcss.com/vue/2.3.0/vue.js");
+AppAsset::addScript($this,"http://cdn.bootcss.com/vue/2.3.0/vue.js");
 ?>
 <style>
     .detail-box {
@@ -23,6 +23,7 @@ use yii\helpers\Url;
         border: 1px solid rgb(192, 200, 212);
         box-shadow: 0px 3px 5px rgb(170, 178, 189);
         margin-top: 13px;
+        padding: 0;  
     }    
 
     .showback {
@@ -35,68 +36,65 @@ use yii\helpers\Url;
         margin-left: auto!important;
         margin-right: auto!important;
     }
+    
+    .test {
+    height: 100%;
+    border-left: 1px solid #eee;
+}
+.navin {
+    line-height: 45px !important;
+}
+.navout .layui-this a {
+    font-weight: bold;
+    color: black;
+}
+
+.navout .layui-this::after {
+    height:3px !important;
+}
+
+.navin a {
+    font-weight: bold;
+}
+.navin a:hover {
+    color:black !important;
+    text-decoration: none;
+}
+
+.list-group-item {
+    border:0px
+}
+.navout .layui-nav-bar {
+    top:42px !important;
+    height: 3px !important;
+}
+
+.navout {
+    border-bottom: 1px solid rgb(238, 238, 238);
+background-color: rgb(255, 255, 255);
+border-radius: 0px;margin-bottom: 0px
+}
 </style>
 
 <div class="fly-panel detail-box" >    
     <div class="showback" >        
         
-<div class="">
-
-    <div class="row">
-
-        <div class="col-md-12">
-
-            <div class="main layui-clear">
-                <div class="wrap">
-                    <div class="">
-                        <div class="fly-tab fly-tab-index" id="example">
-                            <span>
-                                <a href="jie/index.html">全部</a>
-                                <a href="jie/index.html">最新</a>
-                                <a href="jie/index.html">优质</a>
-                                <a href="user/index.html">关注</a>
-                            </span>
-                            <form action="http://cn.bing.com/search" class="fly-search">
-                                <i class="iconfont icon-sousuo"></i>
-                                <input class="layui-input"  autocomplete="off" placeholder="搜索内容，回车跳转" type="text" name="q">
-                            </form>
-                            <a href="jie/add.html" class="layui-btn jie-add">发布问题</a>
-                        </div>
-
-                        
-                        <div >
-<!--                    </div><button v-on:click="nameSearch()">查询</button>-->
-                        <ul class="fly-list" id="postList">
-                            <div id='layTest'></div>
-<!--                            <li class="fly-list-li" v-for="line in lines" style="height:auto">
-                                <a href="user/home.html" class="fly-list-avatar" style="top: 35px;">
-                                      <img src="images/avatar/default.png" alt="">
-                                    </a>
-                                    <h2 class="fly-tip">
-                                      <a v-bind:href="line.url">{{line.title}}</a>
-                                    </h2>
-                                    <p>
-                                      <span class="glyphicon glyphicon-user" aria-hidden='true'></span><em>{{line.nickname}}</em>
-                                      <span class="glyphicon glyphicon-time" aria-hidden='true'></span><em>{{line.create_time}}</em>
-                                      <span class="glyphicon glyphicon-tag" aria-hidden="true"></span>
-                                          {{line.tags}}
-                                      <span class="fly-list-hint"> 
-                                          <a v-bind:href="line.url+'#comments'">评论 {{line.commentCount}}</a> | 最后修改于
-                                                                                     <?php // date('Y-m-d H:m:s','{{line.update_time}}')?>
-                                        <i class="iconfont" title="回答">&#xe60c;</i> 8
-                                        <i class="iconfont" title="人气">&#xe60b;</i> 106
-                                      </span>
-                                    </p>
-                                  </li>-->
-                        </ul>
-                                                        
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div style=""><ul class="layui-nav navout" lay-filter="" style="">
+                <li class="layui-nav-item navin"><a href="">USD/JAP</a></li>
+                <li class="layui-nav-item layui-this navin"><a href="">USD/AUD</a></li>
+                <li class="layui-nav-item navin"><a href="">USD/EUR</a></li>
+                <li class="layui-nav-item navin">
+                  <a href="javascript:;">USD/NZD</a>
+                </li>
+                <li class="layui-nav-item navin"><a href="">GOLD</a></li>
+                <li class="layui-nav-item navin"><a href="">BMW</a></li>
+              </ul>
         </div>
         
-<!--        <div class="col-md-3">
+        
+    <div class="row">
+        <div class="col-md-3 ">
+            <div style="padding:15px;padding-right:0px">
             <div class="searchbox">
                 <ul class="list-group">
                     <li class="list-group-item">
@@ -128,19 +126,68 @@ use yii\helpers\Url;
                     <li class="list-group-item">搜索框</li>
                 </ul>
             </div>
-            
-        </div>-->
+            </div>
+        </div>
+        
+        <div class="col-md-9 test" style="min-height:768px">
+
+            <div class="main layui-clear">
+                <div class="wrap">
+                    <div class="" style="padding:20px">
+                        <div class="fly-tab fly-tab-index" id="example">
+                            <span>
+                                <a href="jie/index.html">全部</a>
+                                <a href="jie/index.html">最新</a>
+                                <a href="jie/index.html">优质</a>
+                                <a href="user/index.html">关注</a>
+                            </span>
+                            <a href="jie/add.html" class="layui-btn jie-add">投稿</a>
+                        </div>
+
+                        
+                        <div >
+<!--                    </div><button v-on:click="nameSearch()">查询</button>-->
+                        <ul class="fly-list" id="postList">
+                            <!--<div id='layTest'></div>-->
+                            <li class="fly-list-li" v-for="line in lines" style="height:auto">
+                                <a href="user/home.html" class="fly-list-avatar" style="top: 35px;">
+                                      <img src="images/avatar/default.png" alt="">
+                                    </a>
+                                    <h2 class="fly-tip">
+                                      <a v-bind:href="line.url">{{line.title}}</a>
+                                    </h2>
+                                    <p>
+                                      <span class="glyphicon glyphicon-user" aria-hidden='true'></span><em>{{line.nickname}}</em>
+                                      <span class="glyphicon glyphicon-time" aria-hidden='true'></span><em>{{line.create_time}}</em>
+                                      <span class="glyphicon glyphicon-tag" aria-hidden="true"></span>
+                                          {{line.tags}}
+                                      <span class="fly-list-hint"> 
+                                          <a v-bind:href="line.url+'#comments'">评论 {{line.commentCount}}</a> | 最后修改于
+                                                                                     <?php // date('Y-m-d H:m:s','{{line.update_time}}')?>
+                                        <i class="iconfont" title="回答">&#xe60c;</i> 8
+                                        <i class="iconfont" title="人气">&#xe60b;</i> 106
+                                      </span>
+                                    </p>
+                                  </li>
+                        </ul>
+                                                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
 
     </div>         
 
-</div>
 
        
         </div>
     </div>
 
 <script id="demo" type="text/html">
-
+<!--
 {{#  layui.each(d.lines, function(index, item){ }}
 
 <li class="fly-list-li"  style="height:auto">
@@ -167,12 +214,41 @@ use yii\helpers\Url;
       {{#  }); }}
       {{#  if(d.lines.length === 0){ }}
   无数据
-{{#  } }} 
+{{#  } }} -->
 </script>
 <?php $this->beginBlock('test') ?>  
             
 
-var laytpl = layui.laytpl;
+$(function(){
+index = layer.load(1,{offset: ['50%', '58%'],anim:4});
+new Vue({
+    el: '#postList',
+        data: {
+            lines: []
+        },
+        mounted:  function () {
+            var self = this;
+            $.ajax({
+               type:"get",
+
+               url:"?r=post/getlist",
+               data:{limit:10,offset:0},
+               success:function(data){
+                        self.lines=JSON.parse(data);            
+                        layer.close(index);
+                      }
+               });                       
+         }
+
+    });
+})
+
+
+<?php $this->endBlock() ?>  
+<?php $this->registerJs($this->blocks['test'], \yii\web\View::POS_END); ?>  
+
+
+<!--var laytpl = layui.laytpl;
 
 function get_List(){           
     $.ajax({
@@ -186,37 +262,11 @@ function get_List(){
             });          
     return things;
 }
-index = layer.load(1);
+index = layer.load(1,{offset: ['50%', '58%'],anim:5});
 var data = get_List();
                
 var getTpl = demo.innerHTML;
 laytpl(getTpl).render(data, function(html){
     layTest.innerHTML = html;
   });
-layer.close(index);
-
-
-
-<?php $this->endBlock() ?>  
-<?php $this->registerJs($this->blocks['test'], \yii\web\View::POS_END); ?>  
-
-<!--$(function(){
-new Vue({
-    el: '#postList',
-        data: {
-            lines: []
-        },
-        mounted:  function () {
-            var self = this;
-            $.ajax({
-               type:"get",
-               url:"?r=post/getlist",
-               data:{limit:10,offset:0},
-               success:function(data){
-                        self.lines=JSON.parse(data);                        
-                      }
-               });                       
-         }
-
-    });
-})-->
+layer.close(index);-->
